330003d0fb2bcc1297900683a52b48580aefac86
nontprawitch <nontprawitch.saetang@gmail.com>
2026-01-13 13:27:07 +0700

    delete folder route copy

--- backend/routes copy/comment.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const commentController = require("../controllers/comment.controller");
-const {authMiddleware} = require("../middlewares/auth.middleware")
-
-router.use(authMiddleware)
-router.post("/addComment", commentController.addComment);
-
-module.exports = router;
--- backend/routes copy/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/user.controller");
-
-router.post("/resgistination", userController.registration);
-router.post("/login", userController.login);
-router.post("/logout", userController.logout);
-
-module.exports = router;
