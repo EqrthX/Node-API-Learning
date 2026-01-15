deb3c06d261da4507ccccffa9c546d943dec692a
nontprawitch <nontprawitch.saetang@gmail.com>
2026-01-15 14:06:14 +0700

    aiven

--- backend/connection.js
+++ backend/connection.js
@@ -2,11 +2,17 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   // ถ้ามีค่าใน env ให้ใช้ค่าจาก env, ถ้าไม่มีให้ใช้ host.docker.internal (สำหรับ docker run), หรือ localhost (สำหรับรันสด)
-  host: process.env.DB_HOST || "localhost", 
+  host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "node_test_api",
-  port: process.env.DB_PORT || "3306"
+  port: process.env.DB_PORT || "3306",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = db;  
